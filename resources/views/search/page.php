4c50d5cbbf67be766a02815e6ebdfdfb4f627234
Michelangelo Lopes <lopsmikelangelo@gmail.com>
2018-05-12 18:36:42 +0200

    Esempio html

--- resources/views/search/page.php
+++ resources/views/search/page.php
@@ -1,2 +1,19 @@
 <?php
-phpinfo();
+
+echo "<html>
+<body>
+
+<h3><mark>Nome</h3>
+<input name=\"Nome\" type=\"text\" value=\"\" size=\"40\" maxlength=\"200\" />
+
+<h3><mark>Email</h3>
+<input name=\"Email\" type=\"text\" value=\"\" size=\"40\" maxlength=\"200\" />
+
+<br>
+<br>
+<button>Invio</button>
+
+</body>
+</html>"
+
+?>
