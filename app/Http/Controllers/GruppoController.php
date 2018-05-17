8144d1018c2361bd7275ff2781414ad747cf045f
Michelangelo Lopes <lopsmikelangelo@gmail.com>
2018-05-17 15:17:07 +0200

    Aggiornamenti pagina group create

--- app/Http/Controllers/GruppoController.php
+++ app/Http/Controllers/GruppoController.php
@@ -13,7 +13,7 @@ class GruppoController extends Controller
      */
     public function index()
     {
-        //
+        return view('groups.create');
     }
 
     /**
--- resources/views/groups/create.blade.php
+++ resources/views/groups/create.blade.php
@@ -2,40 +2,29 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>CORMAN </title>
+    <title>Index Page</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
     <script>
-        function incrementa() {
-            var node = document.createElement("LI");
+        function myFunction() {
+            var node = document.createElement("OL");
             var textnode = document.createTextNode("Email");
             node.appendChild(textnode);
             document.getElementById("myList").appendChild(node);
         }
     </script>
 </head>
-
-<body>
 <div class="container">
-    <h2>Create A Product</h2><br  />
-</div>
-<form method="get" action "{{url('group\create')}}">
-<ul id="myList">
-</ul>
-<table>
-    <tr>
-        <td>Inserire l'email dell'utente da aggiungere al gruppo. </td>
-    <tr>
-    <tr>
-    <tr>
-    <tr>
+
+    <div>
         <td>Email</td>
         <td><input type="text" id="numero" value= "" /></td>
-        <td> <input type="submit" value="+" onclick="incrementa()" />
-        </td>
-    </tr>
-</table>
+        <button onclick="myFunction()">+</button>
+    </div>
+    <div id="myList">
+      <!--   <ol>Email</ol>
 
-</body>
-</html>
-
+       </div>
+   </div>
+   </body>
+   </html>
