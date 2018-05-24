a6092135ebce8e3b339b9bb3916aa6d3912fc2b5
Michelangelo Lopes <lopsmikelangelo@gmail.com>
2018-05-24 15:05:48 +0200

    Modifiche groups create

--- resources/views/groups/create.blade.php
+++ resources/views/groups/create.blade.php
@@ -7,24 +7,25 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
     <script>
         function myFunction() {
-            var node = document.createElement("OL");
-            var textnode = document.createTextNode("Email");
-            node.appendChild(textnode);
-            document.getElementById("myList").appendChild(node);
-
+            var divMail = document.getElementById("myList");
+            var lastChild = divMail.lastElementChild;
+            var x = document.createElement("INPUT");
+            x.setAttribute("type", "text");
+            x.setAttribute("value","");
+            var node = document.createElement("P");
+            node.appendChild(x);
+            divMail.appendChild(node);
         }
     </script>
 </head>
 <div class="container">
+        <div>
+            <button onclick="myFunction()" class = "btn btn-success">+ Aggiungi Mail</button>
+        </div>
 
-    <div>
-        <td>Email</td>
-        <td><input type="text" id="1" value= "" /></td>
-        <button onclick="myFunction()">+</button>
+    <div id="myList" class="align-items-baseline">
+        <p><input type="text" id="email0"></p>
     </div>
-    <div id="myList">
-
-       </div>
-   </div>
-   </body>
-   </html>
+</div>
+</body>
+</html>
