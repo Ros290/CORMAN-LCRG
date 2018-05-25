c34f7f7f04e0499a4eef710fcee57da2174b050a
LezardValeth96 <gpl.croce@gmail.com>
2018-05-25 11:26:02 +0200

    aggiornamento modifica mio profilo
    Ho permesso la modifica dei dati, ma non ho capito come riportare i dati modificati alla pagina del profilo.
    Al momento non posso finire, entro stasera penso di risolvere.

--- resources/views/search/my_profile_edit.blade.php
+++ resources/views/search/my_profile_edit.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Modifics il mio profilo </title>
+    <title>Modifica il mio profilo </title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
@@ -17,8 +17,12 @@
         </ul>
     </div><br/>
 @endif
-    <p>Nome : {{$utente->name}}</p><br>
-    <p>E-Mail : {{$utente->email}}</p><br>
+    <p>Nome : <textarea name="testo" rows="1" cols=30">
+         {{$utente->name}}
+        </textarea></p><br>
+    <p>E-Mail : <textarea name="testo" rows="1" cols=30">
+         {{$utente->email}}
+        </textarea></p><br>
     <h1> Descrizione </h1>
     <form method="post" action={{url('utente/'.$utente->id)}}>
         {{csrf_field()}}
@@ -26,9 +30,8 @@
         <textarea name="testo" rows="15" cols=150">
          {{$utente->description}}
         </textarea>
-        <button type="submit" class="btn btn-success" style="margin-left:38px">Salva Descrizione</button>
     </form>
-    <a href="{{url('utente/'.$utente->id.'/edit')}}" class="btn btn-success">Modifica</a>
+    <a href="{{url('edit/'.$utente->id.'/edit')}}" class="btn btn-success">Salva cambiamenti</a>
 </div>
 </body>
 </html>
